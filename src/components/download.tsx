310c0eeea02ddfa687d569c1f9abd2681f070343
Mauro Balades <mauro.balades@tutanota.com>
2024-07-12 19:45:31 +0200

    refactor: Update download.tsx to use "WindowsStubInstaller" instead of "WindowsInstaller" in select options

--- src/components/download.tsx
+++ src/components/download.tsx
@@ -100,7 +100,7 @@ export default function DownloadPage() {
                               <SelectGroup>
                                 <SelectLabel>Operating System</SelectLabel>
                                 <SelectItem value="WindowsInstaller">Windows Installer</SelectItem>
-                                <SelectItem value="WindowsStubInstaller">Windows Pretty Installer</SelectItem>
+                                {/*<SelectItem value="WindowsStubInstaller">Windows Pretty Installer</SelectItem>*/}
                                 <SelectItem value="WindowsZip">Windows (Zip)</SelectItem>
                                 <SelectItem value="MacOS" disabled>MacOS</SelectItem>
                                 <SelectItem value="Linux">Linux</SelectItem>
