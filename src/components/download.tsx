b897904911e1418e5431ed637811fbd8f3e5133b
Mauro Balades <mauro.balades@tutanota.com>
2024-07-25 13:17:42 +0200

    feat: Add support for flatpak download option on Linux

--- src/components/download.tsx
+++ src/components/download.tsx
@@ -111,6 +111,26 @@ export default function DownloadPage() {
                   )}
                 />
               <Button type="submit">Download Zen 🎉</Button>
+              {form.getValues().platform === "Linux" && (
+                <div className="mt-20 rounded border bg-muted p-4 text-muted-foreground">
+                  <p>
+                    <strong>Linux user?</strong><br/>
+                    We've recently added support for flatpak! You can download the flatpak version by running the following command:
+                    <pre className="bg-white dark:bg-black p-2 rounded mt-4 overflow-auto">flatpak install flathub com.zen.browser</pre>
+                  </p>
+                </div>
+              )}
+              {form.getValues().platform === "MacOS" && (
+                <div className="mt-20 rounded border bg-muted p-4 text-muted-foreground">
+                  <p>
+                    <strong>Mac user?</strong><br/>
+                    Setting up the MacOS version of Zen Browser is a bit more involved. We're working on making this easier, but for now you can follow these steps:
+                    <a href="https://github.com/zen-browser/desktop/issues/53" target="_blank" rel="noreferrer">
+                      <Button className="mt-5">View MacOS instructions</Button>
+                    </a>
+                  </p>
+                </div>
+              )}
             </form>
           </Form>
         </div>
