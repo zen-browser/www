56a65209777830ca3b13a521ddfdb8a53db4f6ad
Mauro Balades <mauro.balades@tutanota.com>
2024-08-11 23:13:49 +0200

    refactor: Improve responsiveness of Features component layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import { Navigation } from "@/components/navigation";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-start">
+    <main className="flex min-h-screen overflow-x-hidden flex-col items-center justify-start">
       <Header />
       <Features />
       <Footer />  
