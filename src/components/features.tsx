48015647729b33a058a5a853ed882e258d70b1f9
Mauro Balades <mauro.balades@tutanota.com>
2024-08-17 22:36:05 +0200

    chore: Update Features component to include responsive layout for larger screens

--- src/components/features.tsx
+++ src/components/features.tsx
@@ -68,7 +68,7 @@ export default function Features() {
           </p>
           <Button className="mt-8" onClick={() => window.location.href = "/download"}>Try it now</Button>
         </div>
-        <div className="relative hidden lg:block flex h-[500px] w-full max-w-lg items-center justify-center overflow-hidden">
+        <div className="relative hidden lg:flex h-[500px] w-full max-w-lg items-center justify-center overflow-hidden">
          <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10">
             <Image src="/logos/zen-black.png" width={100} height={100} alt="Zen Logo" />
          </span>
