5441ed2741ff429bf258845670b275443283790f
Mauro Balades <mauro.balades@tutanota.com>
2024-08-17 22:57:47 +0200

    chore: Update mobile navigation component to hide logo and button on small screens

--- src/components/mobile-nav.tsx
+++ src/components/mobile-nav.tsx
@@ -19,10 +19,10 @@ export function MobileNav() {
       <Sheet open={open} onOpenChange={setOpen}>
          <SheetTrigger asChild>
             <div className="z-40 flex w-full items-center space-between sm:hidden">
-               <Logo className="size-6 ml-4 sm:hidden" />
+               <Logo className="size-6 ml-4" />
                <Button
                   variant="ghost"
-                  className="mr-2 px-0 ml-auto text-base hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 sm:hidden"
+                  className="mr-2 px-0 ml-auto text-base hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0"
                >
                   <SidebarOpen className="size-6" />
                   <span className="sr-only">Toggle Menu</span>
