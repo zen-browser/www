76fae0a4fc3cd70cd27b1299ee7b8f41c36b64d1
mauro 🤙 <91018726+mauro-balades@users.noreply.github.com>
2024-08-27 11:46:43 +0000

    added cache for homepage

--- next.config.mjs
+++ next.config.mjs
@@ -17,6 +17,19 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  async headers() {
+    return [
+      {
+        source: "/",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "s-maxage=1, stale-while-revalidate=59",
+          },
+        ],
+      },
+    ]
+  }
 };
 
 export default nextConfig;
