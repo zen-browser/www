5673a2aafe0d3be4bbab4e15d0613fc3ac627a9a
Waled Khatiz <waledkhatiz@waleds-air.lan>
2024-08-31 11:50:23 +1000

    fix: Use system color preference

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,6 @@ export default async function RootLayout({
         <NextIntlClientProvider messages={messages}>
           <ThemeProvider
             attribute="class"
-            defaultTheme="dark"
             enableSystem
             disableTransitionOnChange
           >
