97c65eb89e3d5930e770b4da279de5e9e2ef0b00
Aniruddha Sil <welcometohellv@gmail.com>
2024-09-01 00:26:17 +0530

    Feat(footer): Add social links

---
+++ src/components/Icons/index.tsx
@@ -0,0 +1,3 @@
+export * from "./RedditIcon"
+export * from "./DiscordIcon"
+export * from "./MastodonIcon"
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
 import Logo from "./logo";
 import TextReveal from "./ui/text-reveal";
+import { DiscordIcon, MastodonIcon, RedditIcon } from "./Icons";
 
 export default function Footer() {
   return (
-    <div className="font-medium flex-col md:flex-row px-10 md:px-0 border-t w-full border-grey py-10 mt-10 flex justify-center align-center">
+    <div className="font-medium flex-col items-center px-10 md:px-0 border-t w-full border-grey py-10 mt-10 flex justify-center align-center gap-2">
       Zen Browser © {new Date().getFullYear()} - Made with ❤️ by the Zen team.
       <a
         href={"https://github.com/zen-browser"}
@@ -13,6 +14,26 @@ export default function Footer() {
       >
         Source Code
       </a>
+      <div className="flex items-center gap-4 justify-center mt-1.5">
+        {
+          [{
+            link: "https://reddit.com/r/zen_browser/",
+            icon: <RedditIcon className="w-6 h-6 fill-white" />,
+          },
+          {
+            link: "https://discord.gg/nnShMQzR4b",
+            icon: <DiscordIcon className="w-6 h-6 fill-white" />,
+          },
+          {
+            link: "https://mastodon.social/@zenbrowser",
+            icon: <MastodonIcon className="w-6 h-6 fill-white" />,
+          }].map(({ link, icon }, i) => (
+            <Link href={link} key={i}>
+              {icon}
+            </Link>
+          ))
+        }
+      </div>
     </div>
   );
 }
