f6a1129323843444a0470b6aa1e94e10db6e6c07
Aniruddha Sil <welcometohellv@gmail.com>
2024-09-01 18:45:41 +0530

    refactor(footer): :art: moved social links to a separate file

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,5 @@
+import socialLinks from "@/lib/social-links";
 import Link from "next/link";
-import { DiscordIcon, MastodonIcon, RedditIcon } from "./Icons";
 
 export default function Footer() {
 	return (
@@ -13,7 +13,7 @@ export default function Footer() {
 				Source Code
 			</a>
 			<div className="mt-1.5 flex items-center justify-center gap-4">
-				{links.map(({ link, icon }, i) => (
+				{socialLinks.map(({ link, icon }, i) => (
 					<Link href={link} key={i}>
 						{icon}
 					</Link>
@@ -22,18 +22,3 @@ export default function Footer() {
 		</div>
 	);
 }
-
-const links = [
-	{
-		link: "https://reddit.com/r/zen_browser/",
-		icon: <RedditIcon className="h-6 w-6 fill-white" />,
-	},
-	{
-		link: "https://discord.gg/nnShMQzR4b",
-		icon: <DiscordIcon className="h-6 w-6 fill-white" />,
-	},
-	{
-		link: "https://mastodon.social/@zenbrowser",
-		icon: <MastodonIcon className="h-6 w-6 fill-white" />,
-	},
-];
---
+++ src/lib/social-links.tsx
@@ -0,0 +1,18 @@
+import { DiscordIcon, MastodonIcon, RedditIcon } from "@/components/Icons";
+
+const socialLinks = [
+	{
+		link: "https://reddit.com/r/zen_browser/",
+		icon: <RedditIcon className="h-6 w-6 fill-white" />,
+	},
+	{
+		link: "https://discord.gg/nnShMQzR4b",
+		icon: <DiscordIcon className="h-6 w-6 fill-white" />,
+	},
+	{
+		link: "https://mastodon.social/@zenbrowser",
+		icon: <MastodonIcon className="h-6 w-6 fill-white" />,
+	},
+];
+
+export default socialLinks;
