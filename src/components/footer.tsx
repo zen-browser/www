ce73a5a96eef60a8e5f1cbf0a605bbdaa3a3fa3b
Aniruddha Sil <welcometohellv@gmail.com>
2024-09-01 16:24:51 +0530

    refactor(footer): :art: move links to a const

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,39 +1,39 @@
 import Link from "next/link";
-import Logo from "./logo";
-import TextReveal from "./ui/text-reveal";
 import { DiscordIcon, MastodonIcon, RedditIcon } from "./Icons";
 
 export default function Footer() {
-  return (
-    <div className="font-medium flex-col items-center px-10 md:px-0 border-t w-full border-grey py-10 mt-10 flex justify-center align-center gap-2">
-      Zen Browser © {new Date().getFullYear()} - Made with ❤️ by the Zen team.
-      <a
-        href={"https://github.com/zen-browser"}
-        target="_blank"
-        className="mt-5 md:mt-0 md:ml-2 font-bold"
-      >
-        Source Code
-      </a>
-      <div className="flex items-center gap-4 justify-center mt-1.5">
-        {
-          [{
-            link: "https://reddit.com/r/zen_browser/",
-            icon: <RedditIcon className="w-6 h-6 fill-white" />,
-          },
-          {
-            link: "https://discord.gg/nnShMQzR4b",
-            icon: <DiscordIcon className="w-6 h-6 fill-white" />,
-          },
-          {
-            link: "https://mastodon.social/@zenbrowser",
-            icon: <MastodonIcon className="w-6 h-6 fill-white" />,
-          }].map(({ link, icon }, i) => (
-            <Link href={link} key={i}>
-              {icon}
-            </Link>
-          ))
-        }
-      </div>
-    </div>
-  );
+	return (
+		<div className="border-grey align-center mt-10 flex w-full flex-col items-center justify-center gap-2 border-t px-10 py-10 font-medium md:px-0">
+			Zen Browser © {new Date().getFullYear()} - Made with ❤️ by the Zen team.
+			<a
+				href={"https://github.com/zen-browser"}
+				target="_blank"
+				className="mt-5 font-bold md:ml-2 md:mt-0"
+			>
+				Source Code
+			</a>
+			<div className="mt-1.5 flex items-center justify-center gap-4">
+				{links.map(({ link, icon }, i) => (
+					<Link href={link} key={i}>
+						{icon}
+					</Link>
+				))}
+			</div>
+		</div>
+	);
 }
+
+const links = [
+	{
+		link: "https://reddit.com/r/zen_browser/",
+		icon: <RedditIcon className="h-6 w-6 fill-white" />,
+	},
+	{
+		link: "https://discord.gg/nnShMQzR4b",
+		icon: <DiscordIcon className="h-6 w-6 fill-white" />,
+	},
+	{
+		link: "https://mastodon.social/@zenbrowser",
+		icon: <MastodonIcon className="h-6 w-6 fill-white" />,
+	},
+];
