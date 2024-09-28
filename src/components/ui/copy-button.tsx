d706380409cfff0d25594eec94a62e3d59f0dde1
Zhenya Goroh <zhenyagoroh@gmail.com>
2024-09-29 00:11:26 +0300

    Added CopyButton component

---
+++ src/components/ui/copy-button.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import * as React from "react";
+
+import { Button } from "@/components/ui/button";
+import { CopyIcon } from "@radix-ui/react-icons";
+
+import { useClipboard } from "@/lib/hooks";
+
+const CopyButton = React.forwardRef<
+	React.ElementRef<typeof Button>,
+	React.ComponentPropsWithoutRef<typeof Button> & {
+		valueToCopy: string;
+	}
+>(({ className, valueToCopy, ...props }, ref) => {
+	const copyToClipboard = useClipboard(valueToCopy);
+
+	return (
+		<>
+			<Button
+				ref={ref}
+				variant={"ghost"}
+				size={"icon"}
+				className={className}
+				onClick={copyToClipboard}
+				{...props}
+			>
+				<CopyIcon className="size-4" />
+			</Button>
+		</>
+	);
+});
+
+export { CopyButton };
