029914052b463a950f7227b00020c0266b3585cd
mauro-balades <mauro.balades@tutanota.com>
2024-10-09 20:40:33 +0200

    Refactor marketplace and theme-card components

--- src/components/marketplace.tsx
+++ src/components/marketplace.tsx
@@ -16,7 +16,7 @@ export default function MarketplacePage({ themes }: { themes: ZenTheme[] }) {
 					className="h-fit min-w-52 text-xs text-muted-foreground lg:mb-0"
 					offsetTop={120}
 				>
-					<h1 className="text-4xl font-bold lg:text-7xl">Mods Store</h1>
+					<h1 className="text-4xl font-bold lg:text-7xl">Zen Mods</h1>
 					<p className="mt-4 text-lg text-muted-foreground">
 						Discover and install Mods for Zen Browser.
 					</p>
@@ -28,7 +28,7 @@ export default function MarketplacePage({ themes }: { themes: ZenTheme[] }) {
 					/>
 				</StickyBox>
 			</div>
-			<div className="mt-10 grid w-full grid-cols-1 gap-8 px-5 pt-12 lg:w-1/2 lg:gap-y-16 lg:px-10 xl:w-2/3 xl:grid-cols-2 2xl:w-3/4 2xl:grid-cols-3">
+			<div className="mt-12 grid w-full grid-cols-1 gap-8 px-5 pt-12 lg:w-1/2 lg:gap-y-16 lg:px-10 xl:w-2/3 lg:grid-cols-2 2xl:w-3/4 2xl:grid-cols-3">
 				{getThemesFromSearch(themes, searchInput, tags).map((theme) => (
 					<ThemeCard key={theme.name} theme={theme} />
 				))}
