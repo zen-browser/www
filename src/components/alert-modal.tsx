4dfca7d585ce6ac1e43d46a613b51bf8c9c73d58
Zhenya Goroh <zhenyagoroh@gmail.com>
2024-10-10 01:35:03 +0300

    Redesigned

--- src/components/alert-modal.tsx
+++ src/components/alert-modal.tsx
@@ -19,21 +19,21 @@ export const AlertModal = ({
 		<AlertDialog.Root open={open} onOpenChange={onOpenChange}>
 			<AlertDialog.Portal>
 				<AlertDialog.Overlay className="fixed inset-0 bg-black opacity-40" />
-				<AlertDialog.Content className="fixed left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] rounded-xl bg-surface p-8">
+				<AlertDialog.Content className="fixed left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] rounded-xl bg-surface p-8 max-w-[450px]">
 					<AlertDialog.Title>
-						<div className="mb-6 flex gap-6">
+						<div className="mb-6 flex flex-col items-center gap-4">
 							<span>
-								<ExclamationTriangleIcon className="size-6 text-yellow-500" />
+								<ExclamationTriangleIcon className="size-12 text-yellow-500" />
 							</span>
-							<p className="font-bold text-yellow-500">
+							<p className="font-bold text-yellow-500 text-center">
 								The Flatpak version is not optimized. For optimized versions,
 								please select other formats.
 							</p>
 						</div>
 					</AlertDialog.Title>
-					<div className="flex justify-end gap-4">
-						<AlertDialog.Cancel asChild>
-							<Button variant={"secondary"}>Cancel</Button>
+					<div className="flex gap-4">
+						<AlertDialog.Cancel className="flex-1" asChild>
+							<Button variant={"outline"} className="bg-transparent">Cancel</Button>
 						</AlertDialog.Cancel>
 						<AlertDialog.Action onClick={onConfirm} asChild>
 							<Button variant={"default"} className="">
