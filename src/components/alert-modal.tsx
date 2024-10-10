2a93961671ec459e8a478ecd1922219fea9d77c0
Zhenya Goroh <zhenyagoroh@gmail.com>
2024-10-10 10:25:57 +0300

    Changed text

--- src/components/alert-modal.tsx
+++ src/components/alert-modal.tsx
@@ -19,26 +19,31 @@ export const AlertModal = ({
 		<AlertDialog.Root open={open} onOpenChange={onOpenChange}>
 			<AlertDialog.Portal>
 				<AlertDialog.Overlay className="fixed inset-0 bg-black opacity-40" />
-				<AlertDialog.Content className="fixed left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] rounded-xl bg-surface p-8 max-w-[450px]">
+				<AlertDialog.Content className="fixed left-[50%] top-[50%] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-xl bg-surface p-8">
 					<AlertDialog.Title>
-						<div className="mb-6 flex flex-col items-center gap-4">
+						<div className="mb-2 flex flex-col items-center gap-4">
 							<span>
 								<ExclamationTriangleIcon className="size-12 text-yellow-500" />
 							</span>
-							<p className="font-bold text-yellow-500 text-center">
-								The Flatpak version is not optimized. For optimized versions,
-								please select other formats.
+							<p className="text-center font-bold text-yellow-500">
+								Flatpak will install the Generic version!
 							</p>
 						</div>
 					</AlertDialog.Title>
+					<AlertDialog.Description asChild>
+						<p className="text-center text-sm text-muted-foreground mb-6">
+							Please select other formats if you want to install the Optimized
+							version.
+						</p>
+					</AlertDialog.Description>
 					<div className="flex gap-4">
 						<AlertDialog.Cancel className="flex-1" asChild>
-							<Button variant={"outline"} className="bg-transparent">Cancel</Button>
+							<Button variant={"outline"} className="bg-transparent">
+								Cancel
+							</Button>
 						</AlertDialog.Cancel>
 						<AlertDialog.Action className="flex-1" onClick={onConfirm} asChild>
-							<Button variant={"default"}>
-								Download anyway
-							</Button>
+							<Button variant={"default"}>Download Generic</Button>
 						</AlertDialog.Action>
 					</div>
 				</AlertDialog.Content>
