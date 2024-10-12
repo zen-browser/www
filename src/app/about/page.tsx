89fddf6e7ab807409e5b250957662d36a8301b20
mr. M <mauro.balades@tutanota.com>
2024-10-12 22:17:59 +0200

    Update developer team information in about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -14,16 +14,19 @@ export default function PrivacyPolicy() {
 					{`
 # Main Developer Team
 
+These are the most active developers in the project. Awesome people that make Zen Browser possible, the order is not important, all of them are a big part of the project.
+
 * [**Mauro Baladés**](https://github.com/mauro-balades): Creator, Main Developer, and a funny guy.
 * **Oscar Gonzalez**: Site Reliability Engineer (SRE) and code signing.
+* [**Jan Heres**](https://janheres.eu/): Active contributor and helps with MacOS builds, known as \`mr. macos\`.
+* [**BrhmDev**](https://github.com/BrhmDev): Active contributor with great contributions, known as \`mr. black magic\`.
+* [**Canoa**](https://thatcanoa.org/) Active contributor, and very active in issue handling and website management, known as \`mr. site & benchmarker\`.
+* [**kristijanribaric**](https://github.com/kristijanribaric): Active contributor, known as \`mr. workspaces\`.
+* [**n7itro**](https://github.com/n7itro): Active contributor and release notes writer, known as \`mr. discord\`. (Also discord moderator, but who would want to be that?)
+* [**Bryan Galdámez**](https://josuegalre.netlify.app/): Huge contributor on theme functionalities, known as \`mr. themes\`.
 * [**Jafeth Garro**](https://iamjafeth.com/): Documentation writer.
-* [**Jan Heres**](https://janheres.eu/): Active contributor and helps with MacOS builds.
-* [**Bryan Galdámez**](https://josuegalre.netlify.app/): Huge contributor on theme functionalities
-* [**n7itro**](https://github.com/n7itro): Active contributor.
-* [**Canoa**](https://thatcanoa.org/) Active contributor, and very active in issue handling
-* [**BrhmDev**](https://github.com/BrhmDev): Active contributor with great ideas.
-* [**Larvey**](https://github.com/LarveyOfficial/): AUR maintainer.
-* [**Gunir**](https://github.com/gunir): Active contributor.
+* [**Larvey**](https://github.com/LarveyOfficial/): AUR maintainer, known as \`mr. AUR manager\`.
+* [**Gunir**](https://github.com/gunir): Active contributor, known as \`mr. 🐷\`.
 
 # Many more contributors
 
