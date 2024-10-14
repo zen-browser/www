869e481a70373183be1585448d27149e0ca6cd24
mr. M <mauro.balades@tutanota.com>
2024-10-14 22:48:19 +0200

    refactor(theme-card): Refactor theme-card component
    Refactor the theme-card component to improve code readability and maintainability. Removed the unnecessary Link component and replaced it with a div element with an onClick event handler. This change ensures compatibility with the site injection process. Also, cleaned up the code formatting and removed unused imports.
    
    Closes #254

--- src/components/theme-card.tsx
+++ src/components/theme-card.tsx
@@ -18,74 +18,79 @@ export default function ThemeCard({
   const authorLink = getThemeAuthorLink(theme);
 
 	return (
-		<Link
-      href={`/mods/${theme.id}`}
-      className="rounded-xl"
-	  prefetch={false} >
-			<Card className="select-none h-full flex-col justify-between rounded-xl border-2 border-[transparent] bg-surface transition-all duration-200 hover:border-[rgba(0,0,0,.5)] hover:shadow-lg dark:bg-[#121212] dark:hover:border-[#333]">
-			  <div className="relative m-2 mb-0 hidden aspect-[1.85/1] h-48 overflow-hidden rounded-xl border-2 border-[rgba(0,0,0,.5)] object-cover shadow dark:border-[#333] lg:block lg:h-auto">
-  				<img
-  					src={theme.image}
-  					alt={theme.name}
-  					className="h-full w-full object-cover"
-  				/>
-  			</div>
-  			<div className="w-full p-5">
-  				<CardHeader>
-  				  <CardTitle className="overflow-ellipsis text-start text-xl font-bold">
-    					{theme.name.substring(0, maxNameLen).trim() +
-    						(theme.name.length > maxNameLen ? "..." : "")}
-    				</CardTitle>
-    				<div className="flex min-h-6 flex-wrap gap-2">
-    					{theme.tags?.length ? (
-    						theme.tags.map((tag) => (
-    							<Badge key={tag} variant="secondary">
-    								<TagIcon className="mr-1 h-3 w-3" />
-    								{tag}
-    							</Badge>
-    						))
-    					) : (
-    						<span className="self-center text-xs italic text-gray-500">
-    							No tags available
-    						</span>
-    					)}
-    				</div>
-  				</CardHeader>
-          <CardContent>
-  				  <p className="text-md mt-2 overflow-ellipsis text-start text-muted-foreground">
-    					{theme.description.substring(0, maxDescLen).trim() +
-    						(theme.description.length > maxDescLen ? "..." : "")}
-    				</p>
-  				</CardContent>
-  				<CardFooter className="mt-2 flex">
-          {theme.homepage && (
-						<a
-							href={theme.homepage}
-							className="text-md text-blue-500"
-							target="_blank"
-							rel="noopener noreferrer"
-							onClick={(e) => e.stopPropagation()}
-						>
-							Homepage
-						</a>
-					)}
-					{theme.homepage && authorLink && (
-						<span className="text-md mx-2 text-muted-foreground">·</span>
-					)}
-					{authorLink && (
-						<a
-							href={authorLink}
-							className="text-md text-blue-500"
-							target="_blank"
-							rel="noopener noreferrer"
-							onClick={(e) => e.stopPropagation()}
-						>
-							Author
-						</a>
-					)}
-  				</CardFooter>
-  			</div>
-			</Card>
-		</Link>
+		<Card className="select-none h-full flex-col justify-between rounded-xl border-2 border-[transparent] bg-surface transition-all duration-200 hover:border-[rgba(0,0,0,.5)] hover:shadow-lg dark:bg-[#121212] dark:hover:border-[#333]"
+			onMouseDown={(e) => {
+				// IMPORTANT NOTE: We do NOT use a Link component here because of how zen manages site injection.
+				//   please for the love of god, dont change this to a Link component. Please.
+				//
+				// If you had the tentation to change this to a Link component and saw this comment, please
+				//   increase this number: 1
+				if (e.button !== 0 && e.button !== 1) return;
+				if (e.target instanceof HTMLAnchorElement) return;
+				window.open(`/themes/${theme.id}`, e.button === 1 ? "_blank" : "_self");
+			}}>
+			<div className="relative m-2 mb-0 hidden aspect-[1.85/1] h-48 overflow-hidden rounded-xl border-2 border-[rgba(0,0,0,.5)] object-cover shadow dark:border-[#333] lg:block lg:h-auto">
+				<img
+					src={theme.image}
+					alt={theme.name}
+					className="h-full w-full object-cover"
+				/>
+			</div>
+			<div className="w-full p-5">
+				<CardHeader>
+					<CardTitle className="overflow-ellipsis text-start text-xl font-bold">
+						{theme.name.substring(0, maxNameLen).trim() +
+							(theme.name.length > maxNameLen ? "..." : "")}
+					</CardTitle>
+					<div className="flex min-h-6 flex-wrap gap-2">
+						{theme.tags?.length ? (
+							theme.tags.map((tag) => (
+								<Badge key={tag} variant="secondary">
+									<TagIcon className="mr-1 h-3 w-3" />
+									{tag}
+								</Badge>
+							))
+						) : (
+							<span className="self-center text-xs italic text-gray-500">
+								No tags available
+							</span>
+						)}
+					</div>
+				</CardHeader>
+				<CardContent>
+					<p className="text-md mt-2 overflow-ellipsis text-start text-muted-foreground">
+						{theme.description.substring(0, maxDescLen).trim() +
+							(theme.description.length > maxDescLen ? "..." : "")}
+					</p>
+				</CardContent>
+				<CardFooter className="mt-2 flex">
+				{theme.homepage && (
+					<a
+						href={theme.homepage}
+						className="text-md text-blue-500"
+						target="_blank"
+						rel="noopener noreferrer"
+						onClick={(e) => e.stopPropagation()}
+					>
+						Homepage
+					</a>
+				)}
+				{theme.homepage && authorLink && (
+					<span className="text-md mx-2 text-muted-foreground">·</span>
+				)}
+				{authorLink && (
+					<a
+						href={authorLink}
+						className="text-md text-blue-500"
+						target="_blank"
+						rel="noopener noreferrer"
+						onClick={(e) => e.stopPropagation()}
+					>
+						Author
+					</a>
+				)}
+				</CardFooter>
+			</div>
+		</Card>
 	);
 }
