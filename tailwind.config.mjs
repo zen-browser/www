f45c7b10e40ab17eba69da982ed322da6ac08998
mr. M <mauro.balades@tutanota.com>
2024-11-24 23:52:26 +0100

    Worked on the new rebranding!
    Co-authored-by: taroj1205 <taroj1205@users.noreply.github.com>

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,11 +3,14 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			screens: {
+				'-md': '@media (min-width: 768px)',
+				'-lg': '@media (min-width: 1024px)',
+			},
 			colors: {
 				"paper": "#F2F0E3",
 				"coral": "#F76F53",
 				"dark": "#202020",
-				"button-primary": "#c95d47",
 				"zen-blue": "#6287f5",
 				"zen-green": "#63f78b",
 			},
