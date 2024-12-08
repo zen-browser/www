02942531518f83251590e6801023bcf93c316f0b
mr. M <mauro.balades@tutanota.com>
2024-12-08 13:57:19 +0100

    Worked on the site a bit more
    Co-authored-by: Eveeifyeve <Eveeifyeve@users.noreply.github.com>

---
+++ src/mods.ts
@@ -0,0 +1,31 @@
+export interface ZenTheme {
+	name: string;
+	description: string;
+	image: string;
+	downloadUrl: string;
+	id: string;
+	homepage?: string;
+	readme: string;
+	preferences?: string;
+	isColorTheme: boolean;
+	author: string;
+	version: string;
+	tags: string[];
+	createdAt: Date;
+	updatedAt: Date;
+}
+
+const THEME_API = "https://zen-browser.github.io/theme-store/themes.json";
+
+export async function getAllMods(): Promise<ZenTheme[]> {
+  try {
+    const res = await fetch(THEME_API);
+    const json = await res.json();
+    // convert dict to array
+    const mods = Object.keys(json).map((key) => json[key]);
+    return mods;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
