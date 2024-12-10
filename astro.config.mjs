fcf4e6248d1117627d80a345a351a84c66551f44
eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2024-12-10 20:21:49 +1100

    chore: removed astro cloudflare adapter
    I added wrangler which is only needed for cloudflare pages.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
-
-import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  adapter: cloudflare()
+	integrations: [tailwind()],
 });
