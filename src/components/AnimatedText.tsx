aa5c4f6ad03823ec7a070e2d80abef6d96e098f2
mr. M <mauro.balades@tutanota.com>
2024-12-11 18:40:01 +0100

    feat: integrate React support and add AnimatedText component for enhanced animations

---
+++ src/components/AnimatedText.tsx
@@ -0,0 +1,98 @@
+import { motion, useInView, useAnimation } from "motion/react";
+import { useEffect, useRef, type JSX } from "react";
+
+type AnimatedTextProps = {
+  text: string | string[];
+  el?: keyof JSX.IntrinsicElements;
+  className?: string;
+  once?: boolean;
+  repeatDelay?: number;
+  animation?: {
+    hidden: any;
+    visible: any;
+  };
+};
+
+const defaultAnimations = {
+  hidden: {
+    opacity: 0,
+    y: 20,
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.1,
+    },
+  },
+};
+
+export const AnimatedText = ({
+  text,
+  el: Wrapper = "p",
+  className,
+  once,
+  repeatDelay,
+  animation = defaultAnimations,
+}: AnimatedTextProps) => {
+  const controls = useAnimation();
+  const textArray = Array.isArray(text) ? text : [text];
+  const ref = useRef(null);
+  const isInView = useInView(ref, { amount: 0.5, once });
+
+  useEffect(() => {
+    let timeout: NodeJS.Timeout;
+    const show = () => {
+      controls.start("visible");
+      if (repeatDelay) {
+        timeout = setTimeout(async () => {
+          await controls.start("hidden");
+          controls.start("visible");
+        }, repeatDelay);
+      }
+    };
+
+    if (isInView) {
+      show();
+    } else {
+      controls.start("hidden");
+    }
+
+    return () => clearTimeout(timeout);
+  }, [isInView]);
+
+  return (
+    <Wrapper className={className}>
+      <span className="sr-only">{textArray.join(" ")}</span>
+      <motion.span
+        ref={ref}
+        initial="hidden"
+        animate={controls}
+        variants={{
+          visible: { transition: { staggerChildren: 0.1 } },
+          hidden: {},
+        }}
+        aria-hidden
+      >
+        {textArray.map((line, lineIndex) => (
+          <span className="block" key={`${line}-${lineIndex}`}>
+            {line.split(" ").map((word, wordIndex) => (
+              <span className="inline-block" key={`${word}-${wordIndex}`}>
+                {word.split("").map((char, charIndex) => (
+                  <motion.span
+                    key={`${char}-${charIndex}`}
+                    className="inline-block"
+                    variants={animation}
+                  >
+                    {char}
+                  </motion.span>
+                ))}
+                <span className="inline-block">&nbsp;</span>
+              </span>
+            ))}
+          </span>
+        ))}
+      </motion.span>
+    </Wrapper>
+  );
+};
