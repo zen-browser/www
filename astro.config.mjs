8314256b53d71f50ade514219fbf726345bddafb
NOCanoa <158103554+NOCanoa@users.noreply.github.com>
2024-12-26 23:43:15 +0000

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,10 @@ import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), react()],
+    integrations: [tailwind(), react(), sitemap()],
     site: 'https://zen-browser.app',
 });
