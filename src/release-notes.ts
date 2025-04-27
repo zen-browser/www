f05dda1478bcf2b63f87b7a123b12fc1178234c9
Mr. M <mr.m@tuta.com>
2025-04-28 00:30:26 +0200

    Refactor ReleaseNoteItem component and add Firefox version extraction logic; update twilight release notes with new features and fixes

--- src/release-notes.ts
+++ src/release-notes.ts
@@ -25,3 +25,19 @@ export interface ReleaseNote {
 
 export const releaseNotes: ReleaseNote[] = releaseNotesStable.reverse()
 export { default as releaseNotesTwilight } from './release-notes/twilight.json'
+
+export function getReleaseNoteFirefoxVersion(
+  releaseNote: ReleaseNote,
+): string | null {
+  // Check if "firefox" is on the feature list
+  for (const feature of releaseNote.features || []) {
+    if (feature.toLowerCase().includes('firefox')) {
+      // may be X or X.X or X.X.X
+      const match = feature.match(/(\d+(\.\d+){0,2})/)
+      if (match) {
+        return match[0]
+      }
+    }
+  }
+  return null
+}
