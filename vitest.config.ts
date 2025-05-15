ed2412c35b966943664f132ca9a282d8ef10482a
taroj1205 <taroj1205@gmail.com>
2025-05-16 11:26:14 +1200

    fix(vitest): ignore playwright tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,13 @@
 /// <reference types="vitest" />
 import { getViteConfig } from 'astro/config';
+import { defaultExclude } from 'vitest/config';
 
 export default getViteConfig({
   test: {
+    exclude: [
+      ...defaultExclude,
+      "**/*.spec.ts",
+    ],
     setupFiles: ['./src/tests/vitest.setup.ts'],
     coverage: {
       provider: 'istanbul',
