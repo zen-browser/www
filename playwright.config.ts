0472e714e6b8eea3f562a4b6de6a5e22a8af5f2d
taroj1205 <taroj1205@gmail.com>
2025-05-16 11:42:43 +1200

    fix(playwright.config): simplify web server command by removing CI condition

--- playwright.config.ts
+++ playwright.config.ts
@@ -65,7 +65,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: process.env.CI ? 'npm run start' : 'npm run dev',
+    command: 'npm run dev',
     url: 'http://127.0.0.1:3000',
     reuseExistingServer: !process.env.CI,
   },
