26e219042aecbbaa0f96565b925c3c602cca5c41
taroj1205 <taroj1205@gmail.com>
2025-05-16 13:05:01 +1200

    fix(playwright.config): standardize base URL for local development
    - Updated the base URL in the Playwright configuration from 'http://127.0.0.1:3000' to 'http://localhost:3000' for improved consistency and clarity in local development environments.

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   use: {
-    baseURL: 'http://127.0.0.1:3000',
+    baseURL: 'http://localhost:3000',
     trace: 'on-first-retry',
   },
 
