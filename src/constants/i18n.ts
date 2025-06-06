38ae467118087e72a97fdf19d586c4d0e16a5622
Lâm Tiên Hưng <89993051+gs4lthung@users.noreply.github.com>
2025-06-06 09:59:02 +0700

    Revert "feat: add Vietnamese translator"
    This reverts commit 2fc70266d8970710b1c898cfb06d1b55c7781614.

--- src/constants/i18n.ts
+++ src/constants/i18n.ts
@@ -1,20 +1,18 @@
 const UI_EN = (await import('~/i18n/en/translation.json', { with: { type: 'json' } })).default
 const UI_JA = (await import('~/i18n/ja/translation.json', { with: { type: 'json' } })).default
-const UI_VI = (await import('~/i18n/vi/translation.json', { with: { type: 'json' } })).default
 
 export const i18n = {
   DEFAULT_LOCALE: 'en',
   LOCALES: [
     { label: 'English', value: 'en', ui: UI_EN, intl: 'en-US' },
     { label: '日本語', value: 'ja', ui: UI_JA, intl: 'ja-JP' },
-    {label:'Tiếng Việt', value: 'vi', ui: UI_EN, intl: 'vi-VN' },
   ],
 }
 
 /**
  * Type definition for UI translations based on the English translation
  */
-export type UIProps = typeof UI_EN | typeof UI_JA| typeof UI_VI
+export type UIProps = typeof UI_EN | typeof UI_JA
 
 export const getIntlLocale = (locale: string) => {
   return i18n.LOCALES.find(l => l.value === locale)?.intl
