11ea7c5c412e8154f48ae9f56b61854b698c0461
TTdsgms <tiagofranca730@gmail.com>
2025-07-05 07:05:09 -0300

    feat(i18n): add portuguese translation

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ export default defineConfig({
   site: 'https://zen-browser.app',
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'ja'],
+    locales: ['en', 'pt-br', 'ja'],
     routing: {
       fallbackType: 'rewrite',
       prefixDefaultLocale: false,
