4b43e242b7bb16cb54542e4e16c738bd53ea2bf4
Michael <87752300+michi-onl@users.noreply.github.com>
2025-07-22 16:07:08 +0200

    feat(i18n): add multiple languages, add LanguagePicker for locale selection

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ export default defineConfig({
   site: 'https://zen-browser.app',
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'ja'],
+    locales: ['en', 'zh', 'es', 'de', 'ja'],
     routing: {
       fallbackType: 'rewrite',
       prefixDefaultLocale: false,
