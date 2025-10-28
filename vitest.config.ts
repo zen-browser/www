ab8ae85711b084382c0bff4fa2c432b915dbfb03
mr. m <91018726+mr-cheffy@users.noreply.github.com>
2025-10-28 17:19:45 +0100

    chore: add dependency overrides for various packages in pnpm workspace (#819)
    * chore: add dependency overrides for various packages in pnpm workspace
    
    * chore: add dependency overrides for various packages in pnpm workspace
    
    * chore: Added nvmrc file
    
    * chore: Remove breaking deps
    
    * chore: update Playwright image version and remove Vitest configuration settings

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,4 @@
 /// <reference types="vitest" />
 import { getViteConfig } from 'astro/config'
-import { defaultExclude } from 'vitest/config'
 
-export default getViteConfig({
-  test: {
-    exclude: [...defaultExclude, '**/*.spec.ts'],
-    setupFiles: ['./src/tests/vitest.setup.ts'],
-    coverage: {
-      provider: 'istanbul',
-      reporter: ['text', 'html'],
-    },
-  },
-})
+export default getViteConfig({})
